058c4ac67dd67921d2cce45a8c973afdc6438489
prajwal joshi <prazwal@outlook.com>
2020-01-28 20:33:13 +1100

    updated  menuitems to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch,Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Homepage/>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route  path="/hats" component={HatsPage}/>
+        </Switch>
       </header>
     </div>
   );
