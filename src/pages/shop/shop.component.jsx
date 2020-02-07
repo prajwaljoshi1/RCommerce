9c09b2bc06b9b6f6d37e5dad343e48a4f5d9aeb9
prajwal joshi <prazwal@outlook.com>
2020-02-07 23:54:51 +1100

    started building shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import SHOP_DATA from './shop.data.js';
+
+class ShopPage extends  React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    }
+}
+
+  render() {
+    return (
+      <div>SHOP_PAGE</div>
+    );
+  }
+    
+}
+
+
+export default ShopPage;
