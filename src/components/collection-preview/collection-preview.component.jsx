90a3093e11b1193f7d9feb366a961bcc547625f7
prajwal joshi <prazwal@outlook.com>
2020-02-08 21:48:46 +1100

    continued working on shop component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import react from 'react';
+
+import './collection-preview.styles.scss';
+
+const PreviewCollection = ({title, items}) => {
+
+  <div className="collection-preview">
+<h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {
+        items.map((item) => {
+          <div key="item.id">{item.name}</div>
+        })
+      }
+    </div>
+  </div>
+
+};
+
+export default CollectionPreview;
